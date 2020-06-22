04fa95d77047da17afea551abed212fb0931bc39
fonsiher <ehernandezm@est.ups.edu.ec>
2020-06-22 12:21:45 -0500

    Commit inicial

---
+++ edad.py
@@ -0,0 +1,10 @@
+#Autor: Edwin Hernandez
+#Calcula tu edad de año actual
+
+anio_actual = 2020
+nombre = input("Cuál es tu nombre: ?")
+anio_nacimiento = int(input("En qué año naciste: ?"))
+
+edad = anio_actual - anio_nacimiento
+
+print("\n Hola",nombre,"tu edad en este año será:",edad);
