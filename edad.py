bccb44f7b0484758bce626949b3e6dba5b249fdf
fonsiher <ehernandezm@est.ups.edu.ec>
2020-06-22 12:47:33 -0500

    Commit inicial

--- edad.py
+++ edad.py
@@ -1,10 +1,15 @@
 #Autor: Edwin Hernandez
 #Calcula tu edad de año actual
 
-anio_actual = 2020
-nombre = input("Cuál es tu nombre: ?")
-anio_nacimiento = int(input("En qué año naciste: ?"))
+anio_actual = 2020 
+nombre = input("Cuál es tu nombre?: ") 
+mes_actual = 6
+anio_nacimiento = int(input("En qué año naciste? : "))
+mes_nacimiento = int(input("En qué mes naciste [ 1-12]? : "))
+edad = anio_actual - anio_nacimiento 
+if mes_nacimiento > mes_actual:
+	edad -=1
 
-edad = anio_actual - anio_nacimiento
+print("\n Hola",nombre,"tu edad en este año es:",edad);
 
-print("\n Hola",nombre,"tu edad en este año será:",edad);
+
